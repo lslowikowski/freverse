f7066329551b4a4ef3a8934c09dc571ac827dea6
Leszek Slowikowski <lslowikowski@gmail.com>
2019-03-08 00:18:50 +0100

    2019.0009

--- mainweb.js
+++ mainweb.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var url = require('url');
-//var first_module = require('./first_module.js');
 var dbconnect = require('./dbconnect.js'); 
 var mysql = require('mysql');
 
+//obiekt con zawierające podstawowe informacje dotyczące połączenia
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -11,13 +11,15 @@ var con = mysql.createConnection({
   database: "sakila"
 });
 
+//tworzymy serwer
 http.createServer(function (req, res) {
+  //serwer zwraca dane nagłówkowe  
   res.writeHead(200, {
     "Access-Control-Allow-Origin": "*", "Content-Type": "application/json; charset=UTF-8"
-  }
-  );
-  var q = url.parse(req.url, true).query;  
-  //res.write(first_module.myDateTime());
+  });
+  //parsujemy przekazane parametry wywołania webservice
+  //w obiekcie q znajdą się poszczegóelne parametry wywołania webservice
+  var q = url.parse(req.url, true).query;    
   //res.end(dbconnect.fieldSize(con, q.tableSchema, q.tableName, q.columnName));
   dbconnect.fieldSize(con, q.tableSchema, q.tableName, q.columnName).then((data) => { 
     res.end(data);
