cbf8e2eae64f5eeff8857b6c9908a89e5520c9eb
Leszek Slowikowski <lslowikowski@gmail.com>
2019-03-08 00:10:22 +0100

    2019.0002

--- mainweb.js
+++ mainweb.js
@@ -1,10 +1,13 @@
 var http = require('http');
 var url = require('url');
+var first_module = require('./first_module.js');
+
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
   var txt = q.year + " " + q.month;
+  res.write(first_module.myDateTime());
   res.end(txt);
 }).listen(8080);
 
