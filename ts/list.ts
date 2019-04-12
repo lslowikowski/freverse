6fe2d9061694d19926acdbccbe9c95bab6a43aca
Leszek Slowikowski <lslowikowski@gmail.com>
2019-04-12 02:01:45 +0200

    Add descrition to List class constructor

--- ts/list.ts
+++ ts/list.ts
@@ -12,15 +12,15 @@ class List{
     /**
      * 
      * @param webservice - webserwice address with port ex. "http://localhost:8080"
-     * @param databaseName - daabase name ex. "sakila"
-     * @param dataContainerId - html id param where data from webservice will be presented, ex. "dataContainerId"
+     * @param databaseName - database name ex. "sakila"
+     * @param dataContainerId - html id param where data from webservice will be presented, ex. "dataContainerId" <div id="dataContainerId" class="tableContainer" style="display:none">
      * @param tableDataTemplate - template file with path for data presentation ex. "templates/tableDataTemplate.html"
-     * @param tableDataTemplateId - id of <table> tag in template file where data twill be present ex. "tableDataTemplateId"
-     * @param markedClass - html class name to marked element with clicked record ex. "table-primary"
-     * @param dataBodyClass -  HTML element class where data are showing
-     * @param dataRowClass 
-     * @param dataPrimaryKey 
-     * @param dataId 
+     * @param tableDataTemplateId - id of <table> tag in template file where data will be present ex. "tableDataTemplateId" <table id="tableDataTemplateId" class="table table-hover display" style="width:100%">
+     * @param markedClass - HTML class name to marked element with clicked record ex. "table-primary" <tr data-id="1" class="datarow odd table-primary" role="row">
+     * @param dataBodyClass -  HTML element class where data are showing ex. ".databody" <tbody class="databody" data-pk="aktor_id">
+     * @param dataRowClass - HTML element class where row data are showing ex. ".datarow" <tr data-id="2" class="datarow even" role="row">
+     * @param dataPrimaryKey - HTML element data parameter name where are primary key names ex."data-pk" <tbody class="databody" data-pk="aktor_id">
+     * @param dataId - data param of HTML element with record primary key id ex. "data-id" <tr data-id="521" class="datarow odd table-primary" role="row">
      */
     constructor(webservice: string,
                 databaseName: string,
